26511b06ac0f1d6ad3d84d6c4744ad3f4ccc33af
Pablo Mathamel <45394280+pmathamel@users.noreply.github.com>
2022-01-28 01:28:48 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aci" {
   # cisco-aci user name
   username = "admin"
   # cisco-aci password
-  password = "C1sc0123p"
+  password = "cisco"
   # cisco-aci url
   url      = "https://10.50.2.59"
   insecure = true
